9c6e6555e8a25e8f9dda67780ccdc092de85bcee
Martynas Bardauskas <martynas.bardauskas@gmail.com>
2014-11-08 14:29:41 +0200

    initial commit

---
+++ letsmeet.js
@@ -0,0 +1,72 @@
+Events = new Mongo.Collection("events");
+Comments = new Mongo.Collection("comments");
+
+RSVP = {
+	types: {
+		going: 2,
+		maybe: 1,
+		no: 0
+	},
+	setUserRSVP: function(rsvptype, event) {
+		var userId = Meteor.user()._id;
+		var currentEvent = Events.findOne(event._id);
+		if(typeof currentEvent.users === "undefined") {
+			currentEvent.users = {};
+		}
+		currentEvent.users[userId] = rsvptype;
+
+		Events.update(event._id, currentEvent);
+	}
+
+};
+
+// https://www.meteor.com/try/9
+
+if (Meteor.isClient) {
+
+	Template.eventListTemplate.helpers({
+		events: function() {
+			return Events.find({});
+		}
+	});
+
+	Template.eventFormTemplate.events({
+		"click #create-event": function(event, template) {
+			var title = template.find('input[name=title]');
+			var description = template.find('input[name=description]');
+			Events.insert({
+				title: title.value,
+				description: description.value,
+				creator: Meteor.user().username,
+				createdAt: new Date() // current time
+			});
+			title.value = "";
+			description.value = "";
+		}
+	});
+
+	Template.eventTemplate.events({
+		"click .remove-event": function() {
+			Events.remove(this._id);
+		},
+		'click .rsvp-going': function() {
+			RSVP.setUserRSVP(RSVP.types.going, this);
+		},
+		'click .rsvp-maybe': function() {
+			RSVP.setUserRSVP(RSVP.types.maybe, this);
+		},
+		'click .rsvp-no': function() {
+			RSVP.setUserRSVP(RSVP.types.no, this);
+		}
+	});
+
+	Accounts.ui.config({
+		passwordSignupFields: "USERNAME_ONLY"
+	});
+}
+
+if (Meteor.isServer) {
+	Meteor.startup(function() {
+		// code to run on server at startup
+	});
+}
